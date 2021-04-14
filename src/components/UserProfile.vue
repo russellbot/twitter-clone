a49b4f18a87c18d44b14972faed39668c955e136
Russell Hart <russellbot@gmail.com>
2021-04-14 17:19:41 -0400

    created userprofile component

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div class="user-profile__admin-badge" v-if="user.isAdmin">
+        Admin
+      </div>
+      <div class="user-profile__folower-count">
+        <strong>Followers: </strong> {{ followers }}
+      </div>
+    </div>
+    <div class="user-profile__tweets-wrapper">
+        <div class="user-profile__tweet" v-for="tweet in user.tweets" :key="tweet.id">
+            {{ tweet.content }}
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        username: "_russellhart",
+        firstName: "Russell",
+        lastName: "Hart",
+        email: "russell@gmail.com",
+        isAdmin: true,
+        tweets: [
+            { id: 1, content: 'Twitter is amazing!' },
+            { id: 2, content: "don't forget to subscribe to Russell.tv" }
+        ]
+      },
+    };
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+  mounted() {
+    this.followUser();
+  },
+};
+</script>
+
+<style>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
+}
+
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+}
+
+.user-profile__admin-badge {
+  background: rebeccapurple;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 10px;
+  font-weight: bold;
+}
+
+h1 {
+  margin: 0;
+}
+</style>
